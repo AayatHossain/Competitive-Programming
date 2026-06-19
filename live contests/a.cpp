ba92c77de38a0769bcfd32d6a1d8e41f3a309d5a
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-06-20 03:02:40 +0600

    div2b

--- live contests/a.cpp
+++ live contests/a.cpp
@@ -1,23 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 signed main(){
     int t; cin>>t;
     while(t--){
         int n; cin>>n;
         vector<int> a(n);
-        for(int i = 0 ; i < n; i++){
-            cin>>a[i];
-        }
-        int mn = a[0];
-        int sum = mn;
-        for(int i = 1; i < n; i++){
-            if(a[i] < mn){
-                mn = a[i];
+        vector<int> b(n);
+        for(int i = 0; i < n; i++)cin>>a[i];
+        for(int i = 0; i < n; i++)cin>>b[i];
+
+        int cnt = 0;
+        bool ok = true;
+        for(int i = 0; i < n; i++){
+            if(a[i] <= b[i]) continue;
+            int j = -1;
+            for(int k = i; k < n; k++){
+                if(a[k] <= b[i]){
+                    j = k;
+                    break;
+                }
             }
-            sum += mn;
+            if(j == -1){
+                ok = false;
+                break;
+            }
+            int val = a[j];
+            for(int k = j; k > i; k--){
+                a[k] = a[k-1];
+            }
+            a[i] = val;
+            cnt += j - i;
         }
-        cout<<sum<<endl;
+        if(ok){
+            cout<<cnt<<endl;
+        }else{
+            cout<<-1<<endl;
+        }
     }
     return 0;
 }
