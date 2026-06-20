4b6f64b9aa87bd67d8326dc08a3b13ff9de130ac
Md. Aayat Hossain Mridha <ah686634@gmail.com>
2026-06-20 12:42:50 +0600

    s

--- live contests/a.cpp
+++ live contests/a.cpp
@@ -1,41 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-signed main(){
-    int t; cin>>t;
-    while(t--){
-        int n; cin>>n;
+#define int long long
+signed main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
         vector<int> a(n);
-        vector<int> b(n);
-        for(int i = 0; i < n; i++)cin>>a[i];
-        for(int i = 0; i < n; i++)cin>>b[i];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
 
-        int cnt = 0;
-        bool ok = true;
-        for(int i = 0; i < n; i++){
-            if(a[i] <= b[i]) continue;
-            int j = -1;
-            for(int k = i; k < n; k++){
-                if(a[k] <= b[i]){
-                    j = k;
-                    break;
-                }
+        int sum = a[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (a[i] >= sum)
+            {
+                sum = a[i];
             }
-            if(j == -1){
-                ok = false;
-                break;
+            else
+            {
+                sum += a[i];
             }
-            int val = a[j];
-            for(int k = j; k > i; k--){
-                a[k] = a[k-1];
-            }
-            a[i] = val;
-            cnt += j - i;
         }
-        if(ok){
-            cout<<cnt<<endl;
-        }else{
-            cout<<-1<<endl;
-        }
+        cout << sum << endl;
     }
     return 0;
 }
